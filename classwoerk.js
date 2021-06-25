901dc2949aa2d6668ecc5c5a36a735ca07304f91
RikHamale100 <r3ezyr3e@gmail.com>
2021-06-25 11:59:59 +0100

    wk4js

---
+++ classwoerk.js
@@ -0,0 +1,65 @@
+var person = 'Ngozi';
+var age = 50;
+var rightWrong = false;
+var numberChildren = null;
+var definition = undefined;
+
+
+console.log(person);
+console.log(age);
+console.log(rightWrong);
+console.log(numberChildren);
+console.log(definition);
+
+
+document.getElementById('name').innerHTML = person;
+document.getElementById('number').innerHTML = age;
+document.getElementById('boolean').innerHTML = rightWrong;
+document.getElementById('null').innerHTML = numberChildren;
+document.getElementById('undefined').innerHTML = definition;
+
+//addition operator
+
+var number1 = 9;
+var number2 = 3;
+
+var sum = number1 + number2;
+console.log(sum);
+
+//multiplication operator
+
+var a = 5;
+var b = 2;
+var multiple = a * b;;
+
+
+console.log(multiple)
+
+//subtraction operators
+
+var h = 30;
+var i = 12;
+var subt = h-i;
+
+console.log(subt)
+
+//division
+
+var m = 200;
+var n = 25;
+var division = m/n
+console.log(division)
+
+
+//increment 
+
+var s = 10;
+s++;
+
+console.log(s)
+
+//decrement operators
+var t = 90;
+t--;
+console.log(t);
+
---
+++ script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML ="Paragraph changed"
+}
+
+document.write("Rikki");
+
+alert("Frontend");
+
+console.log(900);
+
+var x = 5;
+var y = 6;
+var y = 10;
+var z = x + y
+
+
+console.log(z)
+var name = 'Rik';  //string dt
+var age = 30;
+var rightWrong = false;
+var numChildren = null;
+var definition;
+
+console.log()
